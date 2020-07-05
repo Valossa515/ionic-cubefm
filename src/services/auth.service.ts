42042d9718468291152cf62d3b7a264a3330be60
Felipe Martins <fe.mmo515@gmail.com>
2020-07-05 12:52:27 -0300

    fazendo o app aproveitar usuario e senha logado na leta inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -21,6 +21,14 @@ export class AuthService {
             responseType: 'text'
         });
     }
+    refreshToken() {
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`, 
+         {},
+         {
+             observe: 'response',
+             responseType: 'text'
+         });
+     }
 
     successfulLogin(authorizationValue : string)
     {
@@ -36,4 +44,5 @@ export class AuthService {
     {
         this.storage.setLocalUser(null);
     }
+    
 }
